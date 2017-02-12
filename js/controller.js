85995c3c181c96ac1e9331a861c7fcbbd0c9b9c2
SirBLT <blt8324@gmail.com>
2017-02-12 14:02:58 -0700

    home page links

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,20 @@
 angular.module("nflArrestApp")
 .controller("footballCtrl", function($scope, nflArrestService) {
-    
-   
+
+
 
     nflArrestService.getPlayer()
         .then(function(response) {
             $scope.players = response;
-         
-        });    
 
-    
+    });
+
+    nflArrestService.getTeams()
+    .then(function(teams){
+        $scope.teams = teams;
+    })    
+
+
 
 
 })
